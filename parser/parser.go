2fc9b2802dffb26fd375abf641cf17ad1db9be82
Adilet Soronov <adiletelf@gmail.com>
2021-12-12 03:39:14 +0700

    Associate TokenType with prefixParseFn | infixParsefn

--- parser/parser.go
+++ parser/parser.go
@@ -7,13 +7,20 @@ import (
 	"github.com/adiletelf/abyss/token"
 )
 
+type (
+	prefixParseFn func() ast.Expression
+	infixParseFn  func(ast.Expression) ast.Expression
+)
+
 type Parser struct {
-	l *lexer.Lexer
+	l      *lexer.Lexer
+	errors []string
 
 	curToken  token.Token
 	peekToken token.Token
 
-	errors []string
+	prefixParseFns map[token.TokenType]prefixParsefn
+	infixParseFns  map[token.TokenType]infixParsefn
 }
 
 func New(l *lexer.Lexer) *Parser {
@@ -119,4 +126,12 @@ func (p *Parser) expectPeek(t token.TokenType) bool {
 		p.peekError(t)
 		return false
 	}
+}
+
+func (p *Parser) registerPrefix(tokenType token.TokenType, fn prefixParseFn) {
+	p.prefixParseFns[tokenType] = fn
+}
+
+func (p *Parser) registerInfix(tokenType token.TokenType, fn infixParseFn) {
+	p.infixParseFns[tokenType] = fn
 }
