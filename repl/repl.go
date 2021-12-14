64428cc52766b996058539f0ef0201085898928a
Adilet Soronov <adiletelf@gmail.com>
2021-12-15 00:05:30 +0700

    Evaluate boolean literals

--- repl/repl.go
+++ repl/repl.go
@@ -5,7 +5,7 @@ import (
 	"fmt"
 	"github.com/adiletelf/abyss/lexer"
 	"github.com/adiletelf/abyss/parser"
-	// "github.com/adiletelf/abyss/token"
+	"github.com/adiletelf/abyss/evaluator"
 	"io"
 )
 
@@ -31,8 +31,11 @@ func Start(in io.Reader, out io.Writer) {
 			continue
 		}
 
-		io.WriteString(out, program.String())
-		io.WriteString(out, "\n")
+		evaluated := evaluator.Eval(program)
+		if evaluated != nil {
+			io.WriteString(out, evaluated.Inspect())
+			io.WriteString(out, "\n")
+		}
 	}
 }
 
