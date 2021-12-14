a3ab3e865acce48c5faa8fcdaa9ae78fc95d4c5c
Adilet Soronov <adiletelf@gmail.com>
2021-12-15 00:54:05 +0700

    Add prefix/infix operators to REPL

--- object/object.go
+++ object/object.go
@@ -6,7 +6,7 @@ import (
 
 const (
 	NULL_OBJ    = "NULL"
-	INTEGET_OBJ = "INTEGER"
+	INTEGER_OBJ = "INTEGER"
 	BOOLEAN_OBJ = "BOOLEAN"
 )
 
@@ -26,7 +26,7 @@ type Integer struct {
 	Value int64
 }
 
-func (i *Integer) Type() ObjectType { return INTEGET_OBJ }
+func (i *Integer) Type() ObjectType { return INTEGER_OBJ }
 func (i *Integer) Inspect() string  { return fmt.Sprintf("%d", i.Value) }
 
 type Boolean struct {
