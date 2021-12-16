c64e1a785574e09b80214d49ec0ee5f506b345a2
Adilet Soronov <adiletelf@gmail.com>
2021-12-17 02:08:26 +0700

    Fix Function::String()

--- object/object.go
+++ object/object.go
@@ -87,7 +87,7 @@ func (f *Function) Inspect() string {
 	out.WriteString(strings.Join(params, ", "))
 	out.WriteString(") {\n")
 	out.WriteString(f.Body.String())
-	out.WriteString("\n")
+	out.WriteString("\n}")
 
 	return out.String()
 }
